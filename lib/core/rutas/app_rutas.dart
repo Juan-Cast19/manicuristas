827c3bfbddfa782128f2bf7f0b19dbd837df9784
Juan Alberto Castillo <jcastillo035@gmail.com>
2026-01-19 21:05:00 -0400

    Creacion de Constantes

---
+++ lib/core/rutas/app_rutas.dart
@@ -0,0 +1,6 @@
+class AppRutas {
+  static const splash = '/';
+  static const login = '/login';
+  static const registro = '/registro';
+  static const recuperarContrasena = '/recuperar-contrasena';
+}
